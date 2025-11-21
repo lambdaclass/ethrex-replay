72627fd24bcd158d766d822a034b7d311726961b
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-11-21 18:21:46 -0300

    typo

--- src/cli.rs
+++ src/cli.rs
@@ -811,7 +811,7 @@ impl EthrexReplayCommand {
                 }
                 #[cfg(feature = "zisk")]
                 {
-                    if backend != Backend::Zisk {
+                    if backend != Backend::ZisK {
                         execution_result = Some(exec(backend, cache.clone()).await);
                     }
                 }
@@ -1281,7 +1281,7 @@ pub async fn replay_custom_l1_blocks(
     }
     #[cfg(feature = "zisk")]
     {
-        if backend != Backend::Zisk {
+        if backend != Backend::ZisK {
             execution_result = Some(exec(backend, cache.clone()).await);
         }
     }
